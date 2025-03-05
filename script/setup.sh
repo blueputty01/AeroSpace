ca17839a6e2c31601600263827490d37edccd9a8
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2025-03-04 22:01:38 -0500

    Add asdf to build script so that bundle still works

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e # Exit if one of commands exit with non-zero exit code
-set -u # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
+set -e          # Exit if one of commands exit with non-zero exit code
+set -u          # Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’ as an error
 set -o pipefail # Any command failed in the pipe fails the whole pipe
 # set -x # Print shell commands as they are executed (or you can try -v which is less verbose)
 
 add-optional-dep-to-bin() {
-    if /usr/bin/which "$1" &> /dev/null; then
-        /bin/cat > ".deps/bin/${2:-$1}" <<EOF
+    if /usr/bin/which "$1" &>/dev/null; then
+        /bin/cat >".deps/bin/${2:-$1}" <<EOF
 #!/bin/bash
 exec '$(/usr/bin/which "$1")' "\$@"
 EOF
@@ -17,14 +17,15 @@ if /bin/test -z "${NUKE_PATH:-}"; then
     /bin/rm -rf .deps/bin
     /bin/mkdir -p .deps/bin
 
+    add-optional-dep-to-bin asdf
     add-optional-dep-to-bin bash not-outdated-bash # build-shell-completion.sh
-    add-optional-dep-to-bin fish # build-shell-completion.sh
-    add-optional-dep-to-bin rustc # build-shell-completion.sh
-    add-optional-dep-to-bin cargo # build-shell-completion.sh
-    add-optional-dep-to-bin brew # install-from-sources.sh
-    add-optional-dep-to-bin bundle # build-docs.sh
-    add-optional-dep-to-bin bundler # build-docs.sh
-    add-optional-dep-to-bin xcbeautify # build-release.sh
+    add-optional-dep-to-bin fish                   # build-shell-completion.sh
+    add-optional-dep-to-bin rustc                  # build-shell-completion.sh
+    add-optional-dep-to-bin cargo                  # build-shell-completion.sh
+    add-optional-dep-to-bin brew                   # install-from-sources.sh
+    add-optional-dep-to-bin bundle                 # build-docs.sh
+    add-optional-dep-to-bin bundler                # build-docs.sh
+    add-optional-dep-to-bin xcbeautify             # build-release.sh
     add-optional-dep-to-bin git
     add-optional-dep-to-bin swift # Use Swift from PATH
 
@@ -48,7 +49,7 @@ xcodebuild() {
     # Function: createItemModels(for:itemModelSource:)
     # Thread:   <_NSMainThread: 0x6000037202c0>{number = 1, name = main}
     # Please file a bug at https://feedbackassistant.apple.com with this warning message and any useful information you can provide.
-    if /usr/bin/which xcbeautify &> /dev/null; then
+    if /usr/bin/which xcbeautify &>/dev/null; then
         /usr/bin/xcodebuild "$@" 2>&1 | xcbeautify --quiet # Only print tasks that have warnings or errors
     else
         /usr/bin/xcodebuild "$@" 2>&1
